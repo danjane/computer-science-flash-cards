db4ab071074a9108489152287e0ae40de93ef3d8
=yeszao <yeszao@qq.com>
2018-01-03 17:18:22 +0800

    分离slider js程序

--- static/general.js
+++ static/general.js
@@ -50,66 +50,7 @@ $(document).ready(function(){
 
     $('.alert').show().delay(1500).fadeOut();
 
-    var mySwiper = new Swiper ('.swiper-container', {
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'fraction'
-        },
-        speed: 200,
-        autoHeight: true,
-        preventClicks: false,
-        preventClicksPropagation: false,
-        hashNavigation: true,
-        scrollbar: {
-            el: '.swiper-scrollbar'
-        }
-    });
-
-    mySwiper.on('click', function() {
-        var self = mySwiper.slides[mySwiper.realIndex];
-
-        var front = self.getElementsByClassName("front")[0];
-        var back = self.getElementsByClassName("back")[0];
-
-        if (isVisible(front)) {
-            front.style.display = 'none';
-            back.style.display = 'block';
-        } else {
-            front.style.display = 'block';
-            back.style.display = 'none';
-        }
-    });
-
-    initInput(mySwiper.realIndex);
-    mySwiper.on('slideChange', function() {
-        initInput(mySwiper.realIndex);
-    });
-
-    function initInput(index) {
-        var swiper = mySwiper.slides[index];
-        var id = swiper.getAttribute('data-id');
-        var known = swiper.getAttribute('data-known');
-        var editLink = document.getElementsByClassName('edit-link')[0];
-        var knownButton = document.getElementsByClassName('known-button')[0];
-
-        document.getElementsByClassName('mark-known')[0].value = id;
-        editLink.href = editLink.getAttribute('data-url') + id;
-
-        var classVal = knownButton.getAttribute("class");
-        if (parseInt(known) === 1) {
-            classVal = classVal.replace(" btn-muted","");
-        } else {
-            classVal = classVal.replace(" btn-muted","");
-            classVal = classVal.concat(" btn-muted");
-        }
-        knownButton.setAttribute("class", classVal );
-    }
-
-
-    var isVisible = function(elem) {
-        return !(elem.offsetWidth <= 0 && elem.offsetHeight <= 0);
-    };
-
+//id = 1 || "id";alert(123);
     // to remove the short delay on click on touch devices
     FastClick.attach(document.body);
 });
